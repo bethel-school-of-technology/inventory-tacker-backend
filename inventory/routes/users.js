a9832d18cfef68e478868766c52b223bc283b794
Kevin Frohlich <kevinfrohlich@gmail.com>
2019-11-10 13:33:26 -0800

    Reworked backend  - took out JWT token.

--- inventory/routes/users.js
+++ inventory/routes/users.js
@@ -2,21 +2,8 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var authService = require('../services/auth');
-const jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 var axios = require('axios');
-
-//Verify Token
-function verifyToken(req, res, next) {
-	const bearerHeader = req.headers['authorization'];
-	if (typeof bearerHeader !== 'undefined') {
-		const bearer = bearerHeader.split(' ');
-		const bearerToken = bearer[1];
-		req.token = bearerToken;
-		next();
-	} else {
-		res.send('Forbidden');
-	}
-}
 
 router.get('/', function(req, res, next) {
 	res.send(JSON.stringify('respond with a resource'));
@@ -57,164 +44,70 @@ router.post('/login', function(req, res, next) {
 	models.users
 		.findOne({
 			where: {
-				Username: req.body.Username
+				Username: req.body.username,
+				Password: req.body.password
 			}
 		})
 		.then((user) => {
-			if (!user) {
-				console.log('User not found');
-				return res.status(401).send(JSON.stringify('Login Failed'));
+			res.setHeader('Content-Type', 'application/json');
+			if (user) {
+				res.send(JSON.stringify(user));
+				//res.redirect('admin');
 			} else {
-				let passwordMatch = authService.comparePasswords(req.body.Password, user.Password);
-
-				if (passwordMatch) {
-					let token = authService.signUser(user);
-					res.cookie('jwt', token);
-					res.send(token);
-				} else {
-					res.status(401);
-					res.send(JSON.stringify('Wrong password. Please go back and try again.'));
-				}
+				res.send('Invalid login!');
 			}
 		});
-});
-
-router.get('/profile', verifyToken, function(req) {
-	jwt.verify(req.token, (err, res) => {
-		if (err) {
-			res.sendStatus(403);
-			res.send('unable to verify');
-		} else {
-			if (user) {
-				res.send(
-					JSON.stringify({
-						FirstName: user.FirstName,
-						LastName: user.LastName,
-						EmployeeNumber: user.EmployeeNumber,
-						Email: user.Email,
-						Username: user.Username
-					})
-				);
-			} else {
-				res.status(401);
-				res.send('Must be logged in');
-			}
-		}
-	});
 });
 
 router.get('/admin', function(req, res, next) {
-	let token = req.cookies.jwt;
-	if (token) {
-		authService.verifyUser(token).then((user) => {
-			if (user.Admin) {
-				models.users
-					.findAll({
-						where: { Deleted: false },
-						raw: true
-					})
-					.then((usersFound) => res.send(JSON.stringify(usersFound)));
-			} else {
-				res.status(401);
-				res.send(JSON.stringify('You are not authorized to access this page'));
-			}
-		});
-	} else {
-		res.send(JSON.stringify('Need to be logged in as an Administator to view this page.'));
-	}
+	models.users
+		.findAll({
+			where: {
+				Deleted: false
+			},
+			raw: true
+		})
+		.then((usersFound) => res.send(JSON.stringify({ usersFound })))
+		.catch((err) => res.send('You are not authorized to access this page'));
 });
 
 router.post('/admin/editUser/:id/delete', function(req, res, next) {
-	let token = req.cookies.jwt;
-	if (token) {
-		authService.verifyUser(token).then((user) => {
-			if (user) {
-				let UserId = parseInt(req.params.id);
-				models.users
-					.update(
-						{
-							Deleted: true
-						},
-						{
-							where: { Userid: UserId }
-						}
-					)
-					.then((result) => res.send(JSON.stringify(result)))
-					.catch((err) => {
-						res.status(400);
-						res.send(
-							JSON.stringify(
-								'There was a problem deleting the user. Please make sure you are specifying the correct id.'
-							)
-						);
-					});
-			}
-		});
-	}
-});
+	let UserId = parseInt(req.params.id);
 
-router.post('/admin/editUser/:id/update', function(req, res, next) {
-	let token = req.cookies.jwt;
-	if (token) {
-		authService.verifyUser(token).then((user) => {
-			if (user) {
-				let UserId = parseInt(req.params.id);
-				models.users
-					.update(
-						{
-							UserId: users.UserId,
-							FirstName: users.FirstName,
-							LastName: users.LastName,
-							EmployeeNumber: users.EmployeeNumber,
-							Email: users.Email,
-							Username: users.Username,
-							Admin: users.Admin
-						},
-						{
-							where: { UserId: UserId }
-						}
-					)
-					.then((result) => res.send(JSON.stringify(result)))
-					.catch((err) => {
-						res.status(400);
-						res.send(
-							JSON.stringify(
-								'There was a problem updating the employee information. Please make sure you are specifying the correct id.'
-							)
-						);
-					});
+	models.users
+		.update(
+			{
+				Deleted: true
+			},
+			{
+				where: { UserId: UserId }
 			}
+		)
+		.then((result) => res.send(JSON.stringify(result)))
+		.catch((err) => {
+			res.status(400);
+			res.send('There was a problem deleting the user. Please make sure you are specifying the correct id.');
 		});
-	}
 });
 
 router.get('/admin/editUser/:id', function(req, res, next) {
-	let token = req.cookies.jwt;
-	if (token) {
-		authService.verifyUser(token).then((user) => {
-			if (user.Admin) {
-				let UserId = parseInt(req.params.id);
-				if (UserId) {
-					models.users.findByPk(parseInt(req.params.id)).then((users) => {
-						res.send(
-							JSON.stringify({
-								UserId: users.UserId,
-								FirstName: users.FirstName,
-								LastName: users.LastName,
-								EmployeeNumber: users.EmployeeNumber,
-								Email: users.Email,
-								Username: users.Username,
-								Admin: users.Admin
-							})
-						);
-					});
-				}
-			} else {
-				res.send(JSON.stringify('Not authorized to view'));
-			}
+	let UserId = parseInt(req.params.id);
+	if (UserId) {
+		models.users.findByPk(parseInt(req.params.id)).then((users) => {
+			res.send(
+				JSON.stringify({
+					UserId: users.UserId,
+					FirstName: users.FirstName,
+					LastName: users.LastName,
+					EmployeeNumber: users.EmployeeNumber,
+					Email: users.Email,
+					Username: users.Username,
+					Admin: users.Admin
+				})
+			);
 		});
 	} else {
-		res.send(JSON.stringify('Admin privileges required'));
+		res.send('Not authorized to view');
 	}
 });
 
