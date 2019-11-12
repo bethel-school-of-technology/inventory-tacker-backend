99d16c3f84c44fa3af633b868d39eefb5c675b98
Kevin Frohlich <kevinfrohlich@gmail.com>
2019-11-12 11:38:50 -0800

    Cleaned up files for deployment

--- inventory/routes/index.js
+++ inventory/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-/* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Money Mower Staff and Inventory Tracker' });
 });
